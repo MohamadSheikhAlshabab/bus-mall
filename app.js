cee99ce232b8589230c61f6a5c497b71c6f58d63
Your Name In Single Quotes <your email here in single quotes>
2020-03-02 15:06:27 +0200

    chartjs

--- app.js
+++ app.js
@@ -28,19 +28,19 @@ var images = [
 ];
 
 var sectionImg = document.querySelector('#click');
-console.log(sectionImg);
+// console.log(sectionImg);
 var firstImage = document.querySelector('#firstImage');
-console.log(firstImage);
+// console.log(firstImage);
 var secondImage = document.querySelector('#secondImage');
-console.log(secondImage);
+// console.log(secondImage);
 var thirdImage = document.querySelector('#thirdImage');
-console.log(thirdImage);
+// console.log(thirdImage);
 var q = "";
 // if (q == "jpg" ||q == "png" ||q == "gif"){
 firstImage.src = `img/${images[0]}`;
-console.log(firstImage.src);
+// console.log(firstImage.src);
 secondImage.src = `img/${images[1]}`;
-console.log(secondImage.src);
+// console.log(secondImage.src);
 thirdImage.src = `img/${images[2]}`;
 console.log(thirdImage.src);
 // }
@@ -88,30 +88,36 @@ function render() {
     console.log(y);
     console.log(z);
 
-    if (x !== y && z) {
-        if (y !== z && x) {
-            if (z !== y && x) {
+    
 
-                firstImages = Select.all[x];
-                console.log(firstImages);
-                secondImages = Select.all[y];
-                console.log(secondImages);
-                thirdImages = Select.all[z];
-                console.log(thirdImages);
+    if (x === y &&  z) {
+        if (y === z &&  x) {
+            if (z == y && x) {
+                console.log("here will be same images");
 
-                firstImage.setAttribute('src', firstImages.imagePath);
-                secondImage.setAttribute('src', secondImages.imagePath);
-                thirdImage.setAttribute('src', thirdImages.imagePath);
-
-                firstImage.setAttribute('alt', firstImages.name);
-                secondImage.setAttribute('alt', secondImages.name);
-                thirdImage.setAttribute('alt', thirdImages.name);
-
-                firstImage.setAttribute('title', firstImages.name);
-                secondImage.setAttribute('title', secondImages.name);
-                thirdImage.setAttribute('title', thirdImages.name);
             }
         }
+    }
+    else{
+        
+        firstImages = Select.all[x];
+        // console.log(firstImages);
+        secondImages = Select.all[y];
+        // console.log(secondImages);
+        thirdImages = Select.all[z];
+        // console.log(thirdImages);
+
+        firstImage.setAttribute('src', firstImages.imagePath);
+        secondImage.setAttribute('src', secondImages.imagePath);
+        thirdImage.setAttribute('src', thirdImages.imagePath);
+
+        firstImage.setAttribute('alt', firstImages.name);
+        secondImage.setAttribute('alt', secondImages.name);
+        thirdImage.setAttribute('alt', thirdImages.name);
+
+        firstImage.setAttribute('title', firstImages.name);
+        secondImage.setAttribute('title', secondImages.name);
+        thirdImage.setAttribute('title', thirdImages.name);
     }
 
 
@@ -127,31 +133,32 @@ var totals = 0;
 function makeChange(event) {
     // render();
     if (totals < 25) {
-        if (event.target.id !== 'click') {
+
+        if (event.target.id === 'click') {
             if (event.target.id === 'firstImage') {
                 firstImages.click++;
-
-                console.log(firstImage);
+                firstImages.veiws++;
+                // console.log(firstImage);
                 // console.log(firstImages);
 
             } else if (event.target.id === 'secondImage') {
                 secondImages.clicks++;
-
-                console.log(secondImage);
+                secondImages.veiws++;
+                // console.log(secondImage);
                 // console.log(secondImage);
             } else if (event.target.id === 'thirdImage') {
                 thirdImages.clicks++;
-
-                console.log(thirdImage);
-                console.log(thirdImages);
-                console.log(thirdImages.veiws);
-                console.log(thirdImages.clicks);
+                thirdImages.veiws++;
+                // console.log(thirdImage);
+                // console.log(thirdImages);
+                // console.log(thirdImages.veiws);
+                // console.log(thirdImages.clicks);
             }
         }
-       
-        firstImages.veiws++;
-        secondImages.veiws++;
-        thirdImages.veiws++;
+
+        // firstImages.veiws++;
+        // secondImages.veiws++;
+        // thirdImages.veiws++;
         totals++;
         render();
 
@@ -159,10 +166,11 @@ function makeChange(event) {
 
         alert("More Than 25 Clicks");
         render2();
+        myChart();
         click.removeEventListener('click', makeChange);
 
     }
-  
+
 }
 
 
@@ -176,4 +184,104 @@ function render2() {
     }
 
 
+}
+
+function myChart() {
+    var ctx = document.getElementById('myChart');
+    // var t =0;
+    // for (var o=0 ; o <images.all.length;o++){
+    //     t =
+
+    // }
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: [
+                "bag",
+                "banana",
+                "bathroom",
+                "boots",
+                "breakfast",
+                "bubblegum",
+                "chair",
+                "cthulhu",
+                "dog-duck",
+                "dragon",
+                "pen",
+                "pet-sweep",
+                "scissors",
+                "shark",
+                "sweep",
+                "tauntaun",
+                "unicorn",
+                "usb",
+                "water-can",
+                "wine-glass",
+
+
+            ],
+            datasets: [{
+                label: '# of Votes',
+                data: ["1" ,"2","3"],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(100, 99, 132, 0.2)',
+                    'rgba(100, 162, 235, 0.2)',
+                    'rgba(100, 206, 86, 0.2)',
+                    'rgba(255, 200, 132, 0.2)',
+                    'rgba(54, 200, 235, 0.2)',
+                    'rgba(255, 200, 86, 0.2)',
+                    'rgba(255, 99, 25, 0.2)',
+                    'rgba(54, 162, 25, 0.2)',
+                    'rgba(255, 206, 25, 0.2)',
+                    'rgba(100, 99, 132, 0.2)',
+                    'rgba(100, 162, 235, 0.2)',
+                    'rgba(100, 206, 86, 0.2)',
+                    'rgba(255, 50, 132, 0.2)',
+                    'rgba(54, 50, 235, 0.2)',
+                    'rgba(255, 50, 86, 0.2)',
+                    'rgba(255, 99, 25, 0.2)',
+                    'rgba(54, 162, 25, 0.2)'
+
+
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(100, 99, 132, 1)',
+                    'rgba(100, 162, 235, 1)',
+                    'rgba(100, 206, 86, 1)',
+                    'rgba(255, 200, 132, 1)',
+                    'rgba(54, 200, 235, 1)',
+                    'rgba(255, 200, 86, 1)',
+                    'rgba(255, 99, 25, 1)',
+                    'rgba(54, 162, 25, 1)',
+                    'rgba(255, 206, 25, 1)',
+                    'rgba(100, 99, 132, 1)',
+                    'rgba(100, 162, 235,1)',
+                    'rgba(100, 206, 86, 1)',
+                    'rgba(255, 50, 132, 1)',
+                    'rgba(54, 50, 235, 1)',
+                    'rgba(255, 50, 86, 1)',
+                    'rgba(255, 99, 25, 1)',
+                    'rgba(54, 162, 25, 1)'
+
+
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
